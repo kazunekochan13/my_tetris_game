9405bf27fa32fcef9d6e9aa54b5ab65b70067370
KazuNekoCHAN13 <kazuneko78d@gmail.com>
2019-01-23 14:45:23 +0800

    figured out the correct relative positions of tetris shapes

--- src/test.py
+++ src/test.py
@@ -11,15 +11,22 @@ win = pygame.display.set_mode((screen_x, screen_y))
 block_width = 42
 block_x = block_width * 6
 block_y = 256
+i_coords = [[-(block_width * 2), 0], [-(block_width), 0], [0, 0], [block_width, 0]]
+j_coords = [[-(block_width), 0], [0, 0], [block_width, 0], [block_width, block_width]]
+l_coords = [[-(block_width), 0], [-(block_width), block_width], [0, 0], [block_width, 0]]
+o_coords = [[-(block_width), 0], [-(block_width), block_width], [0, 0], [0, block_width]]
+s_coords = [[-(block_width), block_width], [0, 0], [0, block_width], [block_width, 0]]
+t_coords = [[-(block_width), 0], [0, 0], [0, block_width], [block_width, 0]]
+z_coords = [[-(block_width), 0], [0, 0], [0, block_width], [block_width, block_width]]
 
 def draw_shapes():
 	i = 0
 	while i < 4:
-		b_x = block_x
-		b_y = block_y + (i * block_width)
-		pygame.draw.rect(win, (128, 0, 0), (b_x, b_y, block_width, block_width)) # basic square
-		pygame.draw.rect(win, (80, 0, 0), (b_x, b_y, block_width, block_width), 2) # basic dark outline
+		pygame.draw.rect(win, (128, 0, 0), (block_x + z_coords[i][0], block_y + z_coords[i][1], block_width, block_width)) # basic square
+		pygame.draw.rect(win, (80, 0, 0), (block_x + z_coords[i][0], block_y + z_coords[i][1], block_width, block_width), 2) # basic dark outline
 		i += 1
+
+
 	
 def draw_background(): # drawing the stripe background
 	win.fill((0, 125, 125))
