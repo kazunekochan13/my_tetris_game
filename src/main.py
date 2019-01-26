1be541b782adfb09288b94a66a9880e2b1435c68
KazuNekoCHAN13 <kazuneko78d@gmail.com>
2019-01-26 18:29:37 +0800

    stops going beyond bottom and sides of screen

--- src/main.py
+++ src/main.py
@@ -1,16 +1,6 @@
 import pygame
-
-import pygame
-
-pygame.init()
-screen_x = 504
-screen_y = 756
-win = pygame.display.set_mode((screen_x, screen_y))
-clock = pygame.time.Clock()
-run = True
-
-block_width = 42
-pieces = []
+from piece import piece
+from piece_coords import piece_coords
 
 def draw_background():
 	win.fill((0, 125, 125))
@@ -23,8 +13,33 @@ def draw_background():
 
 def draw():
 	draw_background()
+	for shape in pieces:
+		shape.draw(win)
 	pygame.display.update()
 
+def update():
+	global piece_focus
+	if not(piece_focus.has_hit_bottom()):
+		piece_focus.update(2)
+	else:
+		pieces.append(piece(block_x, 0, block_width, screen_x, screen_y, piece_coords.i0))
+		piece_focus = pieces[-1]
+		# create a new shape
+		pass
+
+pygame.init()
+screen_x = 504
+screen_y = 756
+win = pygame.display.set_mode((screen_x, screen_y))
+clock = pygame.time.Clock()
+run = True
+
+block_width = 42
+block_x = block_width * 6
+block_y = 256
+pieces = []
+pieces.append(piece(block_x, block_y, block_width, screen_x, screen_y, piece_coords.j0))
+piece_focus = pieces[0]
 while run:
 
 	clock.tick(30)
@@ -32,7 +47,15 @@ while run:
 	for event in pygame.event.get(): # checking for events
 		if event.type == pygame.QUIT:
 			run = False
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_a:
+				piece_focus.update_left()
+			elif event.key == pygame.K_d:
+				piece_focus.update_right()
+			elif event.key == pygame.K_SPACE:
+				piece_focus.rotate()
 
+	#update()
 	draw()
 
 pygame.quit()
--- src/piece.py
+++ src/piece.py
@@ -3,12 +3,15 @@ from piece_coords import piece_coords
 
 class piece():
 
-	def __init__(self, x, y, width, coords):
+	def __init__(self, x, y, width, screen_width, screen_height, coords):
 		self.x = x
 		self.y = y
 		self.width = width
+		self.screen_width = screen_width
+		self.screen_height = screen_height
 		self.coords = coords
 		self.coords_list = coords.value
+		self.find_edges()
 
 	def draw(self, win):
 		i = 0
@@ -17,11 +20,62 @@ class piece():
 			pygame.draw.rect(win, (80, 0, 0), (self.x + (self.coords_list[i][0] * self.width), self.y + (self.coords_list[i][1] * self.width), self.width, self.width), 2) # basic dark outline
 			i += 1
 
+	def find_left_edge(self):
+		self.left = self.x + (self.coords_list[0][0] * self.width)
+
+	def find_right_edge(self):
+		self.right = self.x + (self.coords_list[3][0] * self.width) + self.width
+
+	def find_top_edge(self):
+		temp = self.y + self.coords_list[0][1] * self.width
+		for coord in self.coords_list:
+			if self.y + coord[1] * self.width < temp:
+				temp = self.y + coord[1] * self.width
+		self.top = temp
+
+	def find_bottom_edge(self):
+		temp = self.y + (self.coords_list[0][1] * self.width) + self.width
+		for coord in self.coords_list:
+			if self.y + (coord[1] * self.width) + self.width > temp:
+				temp = self.y + (coord[1] * self.width) + self.width
+		self.bottom = temp
+
+	def find_edges(self):
+		self.find_left_edge()
+		self.find_right_edge()
+		self.find_top_edge()
+		self.find_bottom_edge()
+
+	def has_hit_bottom(self):
+		return self.bottom >= self.screen_height
+
+	def update(self, y):
+		self.y += y
+		self.find_edges()
+
 	def update_left(self):
-		self.x -= self.width
+		if self.left > 0:
+			self.x -= self.width
+			self.find_edges()
 
 	def update_right(self):
-		self.x += self.width
+		if self.right < self.screen_width:
+			self.x += self.width
+			self.find_edges()
+
+	def check_sides(self):
+		run_left = True
+		run_right = True
+		while run_left:
+			if self.left < 0:
+				self.update_right()
+			else:
+				run_left = False
+		while run_right:
+			if self.right > self.screen_width:
+				self.update_left()
+			else:
+				run_right = False
 
 	def rotate(self):
 		if not(self.coords.name == "o0"):
@@ -69,3 +123,5 @@ class piece():
 				self.coords = piece_coords.l0
 
 			self.coords_list = self.coords.value
+			self.find_edges()
+			self.check_sides()
